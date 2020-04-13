784e33db7a14d4779c44744e94c53394e56c9b28
Viren <viren.p2000@gmail.com>
2020-04-14 00:25:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/covid_19tracker/ui/country/CovidCountryDetail.java
@@ -0,0 +1,45 @@
+package com.example.covid_19tracker.ui.country;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.covid_19tracker.R;
+
+public class CovidCountryDetail extends AppCompatActivity {
+
+    TextView tvDetailCountryName, tvDetailTotalCases, tvDetailTodayCases, tvDetailTotalDeaths,
+            tvDetailTodayDeaths, tvDetailTotalRecovered, tvDetailTotalActive, tvDetailTotalCritical;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covid_country_detail);
+
+        // call view
+        tvDetailCountryName = findViewById(R.id.tvDetailCountryName);
+        tvDetailTotalCases = findViewById(R.id.tvDetailTotalCases);
+        tvDetailTodayCases = findViewById(R.id.tvDetailTodayCases);
+        tvDetailTotalDeaths = findViewById(R.id.tvDetailTotalDeaths);
+        tvDetailTodayDeaths = findViewById(R.id.tvDetailTodayDeaths);
+        tvDetailTotalRecovered = findViewById(R.id.tvDetailTotalRecovered);
+        tvDetailTotalActive = findViewById(R.id.tvDetailTotalActive);
+        tvDetailTotalCritical = findViewById(R.id.tvDetailTotalCritical);
+
+
+        // call Covid Country
+        CovidCountry covidCountry = getIntent().getParcelableExtra("EXTRA_COVID");
+
+        // set text view
+        tvDetailCountryName.setText(covidCountry.getmCovidCountry());
+        tvDetailTotalCases.setText(Integer.toString(covidCountry.getmCases()));
+        tvDetailTodayCases.setText(covidCountry.getmTodayCases());
+        tvDetailTotalDeaths.setText(covidCountry.getmDeaths());
+        tvDetailTodayDeaths.setText(covidCountry.getmTodayDeaths());
+        tvDetailTotalRecovered.setText(covidCountry.getmRecovered());
+        tvDetailTotalActive.setText(covidCountry.getmActive());
+        tvDetailTotalCritical.setText(covidCountry.getmCritical());
+
+    }
+}
---
+++ app/src/main/java/com/example/covid_19tracker/ui/home/HomeFragment.java
@@ -0,0 +1,94 @@
+package com.example.covid_19tracker.ui.home;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.covid_19tracker.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class HomeFragment extends Fragment {
+
+    private TextView tvTotalConfirmed, tvTotalDeaths, tvTotalRecovered, tvLastUpdated;
+    private ProgressBar progressBar;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // call view
+        tvTotalConfirmed = root.findViewById(R.id.tvTotalConfirm);
+        tvTotalDeaths = root.findViewById(R.id.tvTotalDeaths);
+        tvTotalRecovered = root.findViewById(R.id.tvTotalRecovered);
+        tvLastUpdated = root.findViewById(R.id.tvLastUpdated);
+        progressBar = root.findViewById(R.id.progress_circular_home);
+
+        //Action bar title
+        getActivity().setTitle("Overview");
+
+        // call Volley
+        getData();
+
+        return root;
+    }
+
+    private String getDate(long millisecond){
+        SimpleDateFormat formatter = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss aaa");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millisecond);
+        return formatter.format(calendar.getTime());
+    }
+
+    private void getData() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+
+        String url = "https://corona.lmao.ninja/all";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressBar.setVisibility(View.GONE);
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+
+                    tvTotalConfirmed.setText(jsonObject.getString("cases"));
+                    tvTotalDeaths.setText(jsonObject.getString("deaths"));
+                    tvTotalRecovered.setText(jsonObject.getString("recovered"));
+                    tvLastUpdated.setText("Last Updated"+"\n"+getDate(jsonObject.getLong("updated")));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.GONE);
+                Log.d("Error Response", error.toString());
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+}
