b41b78db976e5c5cd75ae0baf1eaa25237c0cd5b
Viren <viren.p2000@gmail.com>
2020-04-27 18:04:06 +0530

    Added Navigation Drawer

--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -1,29 +1,91 @@
 package com.example.covid_19tracker;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.covid_19tracker.ui.country.CountryFragment;
+import com.example.covid_19tracker.ui.home.HomeFragment;
+import com.example.covid_19tracker.ui.india.IndiaFragment;
+import com.example.covid_19tracker.ui.timeline.TimelineFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        navView.setItemIconTintList(null);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(navView, navController);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+                break;
+
+            case R.id.nav_country:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CountryFragment()).commit();
+                break;
+
+            case R.id.nav_timeline:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TimelineFragment()).commit();
+                break;
+
+            case R.id.nav_india:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new IndiaFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 }
