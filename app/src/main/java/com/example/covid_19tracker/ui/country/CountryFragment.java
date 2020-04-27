455742b07a50d5dace5b36b83ede909df2f71dc8
Viren <viren.p2000@gmail.com>
2020-04-27 18:36:15 +0530

    Added Navigation Drawer

--- app/src/main/java/com/example/covid_19tracker/ui/country/CountryFragment.java
+++ app/src/main/java/com/example/covid_19tracker/ui/country/CountryFragment.java
@@ -1,6 +1,7 @@
 package com.example.covid_19tracker.ui.country;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
